1dc9d07d409bdfb26847fdc704eca7a63d7de585
Kms <kms@inesa-t.com>
2024-12-27 11:19:13 +0800

    upd(img): ⚡️ 添加屁姐相册

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import Pengpeng from './view/pengpeng/index.js'
 import TianTian from './view/ctiantian/index.js'
 import Hu from './view/hu/index.js'
 import Cai from './view/caicai/index.js'
+import Pi from './view/pi/index.js'
 
 if (module.hot) {
   module.hot.accept()
@@ -19,16 +20,17 @@ class App extends Component {
   render() {
     return (
       <Switch>
-        <Route path='/' exact component={ () => { return <Gallery/> } }/>
-        <Route path='/Guaer' component={ () => { return <Guaer/> } }/>
-        <Route path='/Gen' component={ () => { return <Gen/> } }/>
-        <Route path='/XXie' component={ () => { return <XXie/> } }/>
-        <Route path='/Kms' component={ () => { return <Kms/> } }/>
-        <Route path='/Jingli' component={ () => { return <Jingli/> } }/>
-        <Route path='/Pengpeng' component={ () => { return <Pengpeng/> } }/>
-        <Route path='/CTianTian' component={ () => { return <TianTian/> } }/>
-        <Route path='/Dhu' component={ () => { return <Hu/> } }/>
-        <Route path='/cai' component={ () => { return <Cai/> } }/>
+        <Route path='/' exact component={() => { return <Gallery /> }} />
+        <Route path='/Guaer' component={() => { return <Guaer /> }} />
+        <Route path='/Gen' component={() => { return <Gen /> }} />
+        <Route path='/XXie' component={() => { return <XXie /> }} />
+        <Route path='/Kms' component={() => { return <Kms /> }} />
+        <Route path='/Jingli' component={() => { return <Jingli /> }} />
+        <Route path='/Pengpeng' component={() => { return <Pengpeng /> }} />
+        <Route path='/CTianTian' component={() => { return <TianTian /> }} />
+        <Route path='/Dhu' component={() => { return <Hu /> }} />
+        <Route path='/cai' component={() => { return <Cai /> }} />
+        <Route path='/pi' component={() => { return <Pi /> }} />
       </Switch>
     )
   }
